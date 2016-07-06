faf5cea1a294429f3a625fc48e879b29238d4100
Vinzeebreak <vincenthoulbreque@gmail.com>
2016-07-06 15:01:04 +0200

    Ajout des contrôleurs pour le profil utilisateur

--- src/js/inscription.js
+++ src/js/inscription.js
@@ -56,5 +56,77 @@
                 restrict: 'E',
                 templateUrl: '/html/templates/footer.html'
             }
-    })
+    });
+    
+    
+    ///////////////////////////
+    // CONTROLLER FOR PROFILE//
+    ///////////////////////////
+    
+    
+    app.controller("ProfileController", function(){
+        this.tab = 1;
+
+        this.selectedTab = function(setTab){
+            this.tab = setTab;
+        };
+        this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+        };
+    });
+
+    app.controller('CourseController', function(){
+        this.courses = courses;
+
+    });
+    
+    // variables for tests only (displaying the courses)
+    
+    var courses = [
+        //TODO : Créer des thumbnails !!!
+        {
+            name: "AngularJS",
+            level: 10,
+            description: "Front-End Javascript",
+            coursesToDo: 5,
+            image:'../../images/angularjs.png'
+        },
+        {
+            name: "NodeJS",
+            level: 38,
+            description: "Back-End Javascript",
+            coursesToDo: 8,
+            image:"../../images/nodejs.png"
+        },
+        {
+            name: "Python",
+            level: 0,
+            description: "Langage déjà vu en prépa !",
+            coursesToDo: 2,
+            image:"../../images/python.png"
+        },
+        {
+            name: "HTML 5",
+            level: 56,
+            description: "Website Skeleton",
+            coursesToDo: 0,
+            image:"../../images/html5.png"
+        },
+        {
+            name: "Android",
+            level: 32,
+            description: "Smartphone Applications",
+            coursesToDo: 5,
+            image:"../../images/android-studio.png"
+        },
+        {
+            name:"Git",
+            level: 67,
+            description: "Versioning",
+            coursesToDo: 1,
+            image:"../../images/git.png"
+        }
+    ];
+    
+    
 })();
--- src/js/profile.js
+++ src/js/profile.js
@@ -6,49 +6,9 @@
 (function(){
     var profileApp = angular.module('profileApp', []);
     console.log('debug1');
-
-    profileApp.controller('CourseController', function(){
-        this.courses = courses;
-
-    });
-
-    var courses = [
-        //TODO : Créer des thumbnails !!!
-        {
-            name: "AngularJS",
-            level: 10,
-            description: "Front-End Javascript",
-            coursesToDo: 5,
-            image:'../../images/angularjs.png'
-        },
-        {
-            name: "NodeJS",
-            level: 38,
-            description: "Back-End Javascript",
-            coursesToDo: 8,
-            image:"../../images/nodejs.png"
-        },
-        {
-            name: "Python",
-            level: 0,
-            description: "Langage déjà vu en prépa !",
-            coursesToDo: 2,
-            image:"../../images/python.png"
-        },
-        {
-            name: "HTML 5",
-            level: 56,
-            description: "Website Skeleton",
-            coursesToDo: 0,
-            image:"../../images/html5.png"
-        },
-        {
-            name: "Android",
-            level: 32,
-            description: "Smartphone Applications",
-            coursesToDo: 5,
-            image:"../../images/android-studio.png"
-        }
-    ];
+    
+    
+    
+    
 
 })();

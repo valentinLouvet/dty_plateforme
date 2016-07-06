17a0e1c3549320de73fb00f8eeb69ee7baef4c5a
Vinzeebreak <vincenthoulbreque@gmail.com>
2016-07-06 15:14:22 +0200

    Ajoût du lien vers la page profile.html

--- server.js
+++ server.js
@@ -27,7 +27,12 @@ app.get('/coach/profile/stats', function (req, res) {
 
 app.get('/coach/profile/students', function (req, res) {
     res.sendFile(__dirname + '/src/html/coachProfile/studentsListe.html')
-})
+});
+
+app.get('/user/profile', function(req,res){
+    res.sendFile(__dirname + '/src/html/templates/profile.html')
+});
+
 app.listen(3333, function () {
     console.log("serveur lancé");
 });

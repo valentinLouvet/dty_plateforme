8d549da86a5f7d605e456d1720e3feefed081393
Vinzeebreak <vincenthoulbreque@gmail.com>
2016-07-07 11:02:11 +0200

    Merge branch 'master' of /Users/vincenthoulbreque/WebstormProjects/DTY_montee/dty_plateforme with conflicts.

--- src/js/inscription.js
+++ src/js/inscription.js
@@ -28,6 +28,7 @@
             [65, 59, 80, 81, 56, 55, 40],
             [28, 48, 40, 19, 86, 27, 90]
         ];
+        $scope.scale = 
         $scope.onClick = function (points, evt) {
             console.log(points, evt);
         };
@@ -40,6 +41,7 @@
             ];
         }, 3000);
     }]);
+    
 
     app.controller('viewController',function ($scope, $rootScope) {
         this.tab=1;

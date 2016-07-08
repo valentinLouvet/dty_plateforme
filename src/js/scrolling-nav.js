b3309318a3082eada5e7aa1a03b0a48fc459c40b
Latylus <pierre.lataillade@gmail.com>
2016-07-08 11:13:00 +0200

    Work on collapsible and scrollable panel in Statistics

---
+++ src/js/scrolling-nav.js
@@ -0,0 +1,27 @@
+
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+$(function() {
+    $('a.show-lesson').bind('click', function(event) {
+        var $anchor = $(this);
+        ($anchor.attr('href')).collapse('show');
+    });
+});

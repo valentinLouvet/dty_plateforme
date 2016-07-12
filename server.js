f39495709da011131d1e9965703b282a79e3660e
thraxii <antoinerebeyre78@gmail.com>
2016-07-12 16:48:45 +0200

    modif nav-bar

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/intro', function (req,res) {
 // test de départ
 app.get('/test', function (req, res){
     res.sendFile(__dirname+'/src/html/templates/test_beginning.html')
-})
+});
 
 app.listen(3333, function () {
     console.log("Server launched on port 3333");

fdba2c1e1a5d645244e0328ccac8de36f5c291fa
valentin.louvet <val.louvet@gmail.com>
2016-07-19 14:46:19 +0200

    ajout de ROLE_COACH

--- nouvelle_version/src/main/webapp/app/admin/user-management/user-management.controller.js
+++ nouvelle_version/src/main/webapp/app/admin/user-management/user-management.controller.js
@@ -10,7 +10,7 @@
     function UserManagementController(Principal, User, ParseLinks, $state, pagingParams, paginationConstants) {
         var vm = this;
 
-        vm.authorities = ['ROLE_USER', 'ROLE_ADMIN'];
+        vm.authorities = ['ROLE_USER', 'ROLE_ADMIN', 'ROLE_COACH'];
         vm.currentAccount = null;
         vm.languages = null;
         vm.loadAll = loadAll;
@@ -27,7 +27,7 @@
         vm.transition = transition;
 
         vm.loadAll();
-        
+
         Principal.identity().then(function(account) {
             vm.currentAccount = account;
         });

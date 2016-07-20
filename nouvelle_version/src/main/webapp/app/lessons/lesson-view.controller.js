54adf3d3b70fcae08096da7685f3f4a9e0979e28
Minwog <noemie.gonnier@laposte.net>
2016-07-20 10:53:49 +0200

    Ajout photos et ajout lesson directory

---
+++ nouvelle_version/src/main/webapp/app/lessons/lesson-view.controller.js
@@ -0,0 +1,10 @@
+(function(){
+
+    angular.module('objectifDtyApp')
+        .controller('lessonViewController',['$state','$http',
+
+            ])
+
+
+
+}());
---
+++ nouvelle_version/src/main/webapp/app/lessons/lesson.state.js
@@ -0,0 +1,18 @@
+// Lesson.state : pour tous les états relatifs à la leçon à savoir : view, edit, create. ( edit et create ? )
+(function(){
+
+    angular.module('objectifDtyApp').config(stateConfig);
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider){
+        $stateProvider.state{'lesson',{
+
+        }
+
+        }
+
+    };
+
+
+}());
+

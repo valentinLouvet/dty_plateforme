415eefb471238955ac44adae9021eb881aff3035
Vinzeebreak <vincenthoulbreque@gmail.com>
2016-07-21 14:35:03 +0200

    Merge branch 'master' of /Users/vincenthoulbreque/DTY/dty_plateforme with conflicts.

--- nouvelle_version/src/main/webapp/app/course/create-course/create-course.controller.js
+++ nouvelle_version/src/main/webapp/app/course/create-course/create-course.controller.js
@@ -6,36 +6,185 @@
         .controller('courseCreationController', courseCreationController);
 
     function courseCreationController () {
-        this.compteur= 0;
+
+        this.compteurQuestion = 1;
+        this.compteurAnswer = [0];
         this.newLesson = {
             text : "",
             quizz : []
         };
 
         this.question = {
-            question : "enter question here",
-            id: this.compteur,
+            intitulate : "enter question here",
+            id: this.compteurQuestion,
             answers : []
         };
 
-        this.newLesson.quizz.push(this.question);
-        this.question.answers.push("answer 1");
-        this.question.answers.push("answer 2");
-
-        this.addAnswer = function (i) {
-            this.newLesson.quizz[i].answers.push("answer " + (this.newLesson.quizz[i].answers.length+1) );
+        this.answer = {
+            intitulate : "answer's intitulate",
+            id: this.compteurAnswer[0] + 1
         };
 
-        this.addQuestion = function () {
-            this.compteur= this.compteur+1;
-            var question = {
-                question : "enter question here",
-                id: this.compteur,
-                answers : []
+        // Initialisation de la 1ère question
+        // avec 2 réponses
+
+        var answer1 = {
+            intitulate : "Answer " + (this.compteurAnswer[0] + 1),
+            id: this.compteurAnswer[0] + 1
+        };
+
+        this.compteurAnswer[this.compteurQuestion-1] = this.compteurAnswer[this.compteurQuestion-1] + 1;
+
+        var answer2 = {
+            intitulate : "Answer " + (this.compteurAnswer[0] + 1),
+            id: this.compteurAnswer[0] + 1
+        };
+
+        this.compteurAnswer[this.compteurQuestion-1] = this.compteurAnswer[this.compteurQuestion-1] + 1;
+
+        console.log("New answer created, intitulate : "+this.answer.intitulate+", id : "+this.answer.id);
+        console.log("New answer created, intitulate : "+this.answer.intitulate+", id : "+this.answer.id);
+
+        this.question.answers.push(answer1);
+        this.question.answers.push(answer2);
+
+        this.newLesson.quizz.push(this.question);
+
+        console.log("New question created, intitulate : "+this.question.intitulate+", id : "+this.question.id);
+        console.log("Answers of this question : " + this.question.answers);
+
+        // Différentes fonctions pour ajouter/supprimer les questions/réponses
+
+
+
+        this.addAnswer = function(idOfQuestion) {
+
+            console.log("####################");
+            console.log("idOfQuestion : " + idOfQuestion);
+
+            var answer = {
+                intitulate : "Answer " + (this.compteurAnswer[idOfQuestion - 1] + 1),
+                id : this.compteurAnswer[idOfQuestion - 1] + 1
+
+            }
+
+            this.compteurAnswer[idOfQuestion - 1] = this.compteurAnswer[idOfQuestion - 1] + 1;
+
+            this.newLesson.quizz[idOfQuestion - 1].answers.push(answer);
+
+            console.log("New answer created, intitulate : "+answer.intitulate+", id : "+answer.id);
+
+            console.log("####################");
+
+        };
+
+        this.removeAnswer = function(idOfAnswer, idOfQuestion){
+
+            console.log("####################");
+            console.log("idOfQuestion : " + idOfQuestion);
+
+            if(this.compteurAnswer[idOfQuestion - 1] != 2){
+                // Il doit toujours y avoir au moins 2 réponses à une question
+
+                if(idOfAnswer != this.compteurAnswer[idOfQuestion - 1]){
+                    // Il faut décaler les id de toutes les réponses qui suivent
+
+                    for(var i=idOfAnswer - 1; i<this.compteurAnswer[idOfQuestion - 1]; i++){
+                        this.newLesson.quizz[idOfQuestion - 1].answers[i].id = this.newLesson.quizz[idOfQuestion - 1].answers[i].id - 1;
+                        this.newLesson.quizz[idOfQuestion - 1].answers[i].intitulate = "Answer " + this.newLesson.quizz[idOfQuestion - 1].answers[i].id;
+                    }
+
+                }
+                else{
+                    console.log("Last answer...");
+                }
+                this.newLesson.quizz[idOfQuestion - 1].answers.splice(idOfAnswer - 1, 1);
+                this.compteurAnswer[idOfQuestion - 1] = this.compteurAnswer[idOfQuestion - 1] - 1;
+
+            }
+            else{
+                console.log("Can't delete this answer");
+            }
+
+
+            console.log("####################");
+
+
+
+        }
+
+        this.addQuestion = function() {
+
+            console.log("####################");
+
+
+            this.compteurAnswer.push(0);
+            this.compteurQuestion = this.compteurQuestion + 1;
+
+            var answer1 = {
+                intitulate : "Answer 1",
+                id: this.compteurAnswer[this.compteurQuestion - 1] + 1
             };
-            question.answers.push("answer 1");
-            question.answers.push("answer 2");
+
+            this.compteurAnswer[this.compteurQuestion - 1] = this.compteurAnswer[this.compteurQuestion - 1] + 1;
+
+            var answer2 = {
+                intitulate : "Answer 2",
+                id: this.compteurAnswer[this.compteurQuestion - 1] + 1
+            };
+
+            this.compteurAnswer[this.compteurQuestion - 1] = this.compteurAnswer[this.compteurQuestion - 1] + 1;
+
+            var question = {
+                intitulate : "Question n° " + (this.compteurQuestion),
+                id : this.compteurQuestion,
+                answers : []
+            }
+
+            question.answers.push(answer1);
+            question.answers.push(answer2);
+
             this.newLesson.quizz.push(question);
+
+            console.log("New question created, intitulate : "+question.intitulate+", id : "+question.id);
+            console.log("####################");
+
+        };
+
+        this.removeQuestion = function(idOfQuestion) {
+
+            console.log("####################");
+            console.log("idOfQuestion : " + idOfQuestion);
+
+            // Il faut toujours qu'il y ait au moins une question
+            if(this.compteurQuestion != 1){
+
+                if(idOfQuestion != this.compteurQuestion){
+                    // il faut changer les id de toutes les questions situées après cette question supprimée
+                    // en les décrémentant
+                    console.log("This is not the last question");
+                    for(var i = idOfQuestion - 1 ; i<this.compteurQuestion; i++){
+                        this.newLesson.quizz[i].id = this.newLesson.quizz[i].id - 1;
+                    }
+                }
+                else{
+                    console.log("This is the last question");
+                }
+
+                this.newLesson.quizz.splice(idOfQuestion - 1, 1);
+                this.compteurAnswer.splice(idOfQuestion - 1, 1);
+                this.compteurQuestion = this.compteurQuestion - 1;
+
+                console.log("Question suppressed");
+
+
+            }
+            else{
+                console.log("Can't suppress ! At least a question must remain");
+            }
+
+            console.log("####################");
+
         };
 
         this.removeQuestion = function (i) {
@@ -44,4 +193,6 @@
         }
 
     }
+
+
 })();
--- nouvelle_version/src/main/webapp/app/course/edit-course/edit-course.controller.js
+++ nouvelle_version/src/main/webapp/app/course/edit-course/edit-course.controller.js
@@ -1,68 +1,41 @@
-(function () {
+(function() {
+    'use strict';
 
     angular
         .module('objectifDtyApp')
-        .controller('courseEditionController', courseEditionController);
+        .controller('editCreationController', courseCreationController);
 
-    function courseEditionController() {
-        this.blocs=blocs;
-        this.compteur=1;
+    courseCreationController.$inject = ['Sessions', 'Principal'];
 
-        this.addCompteur= function(i){
-            this.compteur = this.compteur + i
+    function courseCreationController () {
+
+        this.lessons = [];
+        this.editedLesson = {
+            text : "",
+            quizz : []
+        };
+        this.question = {
+            question : "enter question here",
+            answers : []
+        };
+        this.question.answers.push("answer 1");
+        this.question.answers.push("answer 2");
+        this.addAnswer = function () {
+            this.question.answers.push("answer " + (this.question.answers.length+1) );
+        };
+        this.newLesson.quizz.push(this.question);
+        this.addQuestion = function () {
+            this.question = {
+                question : "enter question here",
+                answers : []
+            };
+            this.question.answers.push("answer 1");
+            this.question.answers.push("answer 2");
+            this.addAnswer = function () {
+                this.question.answers.push("answer " + (this.question.answers.length+1) );
+            };
+            this.newLesson.quizz.push(this.question);
         };
 
-        this.setCompteur = function (i) {
-            this.compteur = i
-        }
-
     }
-
-    var blocs = [
-        {
-            name: 'HTML',
-            courses: [
-                {
-                    name: 'Lesson 1 HTML'
-                },
-                {
-                    name: 'Lesson 2 HTML'
-                },
-                {
-                    name: 'Lesson 3 HTML'
-                }
-            ]
-        },
-        {
-            name: 'CSS',
-            courses: [
-                {
-                    name: 'Lesson 1 CSS'
-                },
-                {
-                    name: 'Lesson 2 CSS'
-                },
-                {
-                    name: 'Lesson 3 CSS'
-                },
-                {
-                    name: 'Lesson 4 CSS'
-                }
-            ]
-        },
-        {
-            name: 'Angular',
-            courses: [
-                {
-                    name: 'Lesson 1 Angular'
-                },
-                {
-                    name: 'Lesson 2 Angular'
-                }
-            ]
-        }
-    ]
-
-
 })();
-

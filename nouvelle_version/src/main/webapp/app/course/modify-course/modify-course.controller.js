c761ddebb53284f9ec56699f5dd6a1b382242a4f
thraxii <antoinerebeyre78@gmail.com>
2016-07-25 15:36:31 +0200

    possibilité de modifier la partie lesson des cours

--- nouvelle_version/src/main/webapp/app/course/modify-course/modify-course.controller.js
+++ nouvelle_version/src/main/webapp/app/course/modify-course/modify-course.controller.js
@@ -5,9 +5,19 @@
         .module('objectifDtyApp')
         .controller('courseModifyController',['$cookies', function ($cookies) {
             var vm=this;
-           vm.course=$cookies.getObject('course')
-            console.log(vm.course)
-            console.log(vm.course.questions)
+            vm.modifyCourse= false;
+            vm.course=$cookies.getObject('course');
+            vm.newContent=vm.course.content;
+            console.log(vm.course);
+
+            vm.changeCourse= function () {
+                vm.modifyCourse = !vm.modifyCourse
+            };
+
+            vm.saveChanges = function () {
+                vm.course.content = vm.newContent;
+                vm.modifyCourse=false;
+            }
 
 
         }])

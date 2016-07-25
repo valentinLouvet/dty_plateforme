deb3f68430c2288532dd74a28d82d411555dd5a9
thraxii <antoinerebeyre78@gmail.com>
2016-07-25 16:15:58 +0200

    le texte énorme sur la page d'acceuil ne déborde enfin plus ! Victoire !

--- nouvelle_version/src/main/webapp/app/course/modify-course/modify-course.controller.js
+++ nouvelle_version/src/main/webapp/app/course/modify-course/modify-course.controller.js
@@ -3,7 +3,7 @@
 
     angular
         .module('objectifDtyApp')
-        .controller('courseModifyController',['$cookies', function ($cookies) {
+        .controller('courseModifyController',['$cookies', '$state', 'Lesson', function ($cookies, $state, Lesson) {
             var vm=this;
             vm.modifyCourse= false;
             vm.course=$cookies.getObject('course');
@@ -17,6 +17,11 @@
             vm.saveChanges = function () {
                 vm.course.content = vm.newContent;
                 vm.modifyCourse=false;
+            };
+
+            vm.saveCourse = function () {
+                //$state.go('lesson.edit({id: vm.course.id})')
+                $state.go('lesson.create')
             }
 
 

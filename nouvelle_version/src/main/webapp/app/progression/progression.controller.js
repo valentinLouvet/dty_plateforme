29392ac9eaba6c838b0b5af214b2289136ae68c7
Minwog <noemie.gonnier@laposte.net>
2016-07-26 14:11:34 +0200

    Acces todays lesson via progression ( pas via navbar)

--- nouvelle_version/src/main/webapp/app/progression/progression.controller.js
+++ nouvelle_version/src/main/webapp/app/progression/progression.controller.js
@@ -10,13 +10,21 @@
     function progressionController (Student,$scope, Principal,LoginService, $state) {
         var vm = this;
         var user=Principal.identity().$$state;
-        var student=Principal.getStudent().$$state;
+        var student=Principal.getStudent().then(function (data) {
+            console.log(data)
+        });
 
         vm.blocs=[];
         vm.identity=user;
 
         console.log(user);
         console.log(student)
+
+        Student.query().$promise
+            .then(function(data){
+                vm.student=data[0];
+                console.log(vm.student)
+            })
 
     }
 })();

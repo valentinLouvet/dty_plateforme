db74628b946d98eafbac5edfce96d71c1745fdad
valentin.louvet <val.louvet@gmail.com>
2016-07-27 10:24:58 +0200

    creation d'une lesson done lors du submit des questions

--- nouvelle_version/src/main/webapp/app/course/view-course/view-course.controller.js
+++ nouvelle_version/src/main/webapp/app/course/view-course/view-course.controller.js
@@ -2,28 +2,55 @@
 
     angular.module('objectifDtyApp').controller('CourseViewController',ViewCourseController);
 
-    ViewCourseController.$inject = ['Principal','courseView','$scope', '$rootScope', '$stateParams', 'DataUtils', 'Lesson', 'Coach', 'Bloc', 'Question', 'Lesson_done'];
+    ViewCourseController.$inject = ['Principal','courseView','$scope', '$rootScope', '$stateParams', 'DataUtils', 'Lesson', 'Coach', 'Bloc', 'Question', 'Lesson_done','Student'];
 
-        function ViewCourseController(Principal,courseView,$scope,$rootScope,$stateParams,DataUtils,Lesson,Coach,Bloc,Question,Lesson_done){
+        function ViewCourseController(Principal,courseView,$scope,$rootScope,$stateParams,DataUtils,Lesson,Coach,Bloc,Question,Lesson_done,Student){
             var vm = this;
             vm.id=$stateParams.id;
             vm.lesson=Lesson.get({id: vm.id});
 
             vm.score = null;
+            Student.query().$promise.then(function(data){
+                vm.student = data;
+            });
 
-            console.log(vm.score);
             vm.submitLesson = function(){
                 vm.score = 0;
                 for(i=0; i<vm.lesson.questions.length; i++){
-                    if(vm.lesson.questions[i].score){
+                    if(vm.lesson.questions[i].score == "true"){
                         vm.score++;
                     }
                 }
                 vm.score *= 100;
-                vm.score=vm.score/vm.lesson.questions.length;
+                vm.score/=vm.lesson.questions.length;
 
-                console.log(vm.score);
+                console.log(parseInt(vm.score));
+                vm.lesson_done = {
+                    note_init : parseInt(vm.score),
+                    note_max : parseInt(vm.score),
+                    student : vm.student[0],
+                    lessons : [vm.lesson]
+                };
+                console.log(vm.lesson_done);
+                save();
             };
+            function save () {
+                vm.isSaving = true;
+                if (vm.lesson_done.id !== null) {
+                    Lesson_done.update(vm.lesson_done, onSaveSuccess, onSaveError);
+                } else {
+                    Lesson_done.save(vm.lesson_done, onSaveSuccess, onSaveError);
+                }
+            }
+
+            function onSaveSuccess (result) {
+                $scope.$emit('objectifDtyApp:lesson_doneUpdate', result);
+                vm.isSaving = false;
+            }
+
+            function onSaveError () {
+                vm.isSaving = false;
+            }
 
 
 

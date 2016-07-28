8211fb5b2c79f97207d4a0399a98ba69f3b00ab4
Minwog <noemie.gonnier@laposte.net>
2016-07-28 10:14:38 +0200

    Page progression : affiche les blocs faits. On ne peut pas encore afficher les lessons qui vont avec, j'y travaille.

--- nouvelle_version/src/main/webapp/app/progression/progression.controller.js
+++ nouvelle_version/src/main/webapp/app/progression/progression.controller.js
@@ -10,22 +10,80 @@
     function progressionController (Student,$scope, Principal, LoginService, Lesson_done, $state) {
 
         var vm=this;
-        vm.lesson_dones=[];
+        var Blocs=[];
 
         Principal.getStudent().then(function (data) {
             vm.student=data;
             vm.user=data.user;
 
             Lesson_done.query({},function(lessons){
-                console.log(lessons);
-                for(var i=0;i<lessons.length;i++){
-                    Lesson_done.get({id:lessons[i].id},function (data) {
-                        vm.lesson_dones.push(data);
-                    })
-                }
+                Bloc_done(lessons);
             });
-
-
     });
+        /**
+
+        fonction bloc_done : parcoure les lessons réalisées par le student - et crée le tableau Blocs
+         qui continent des elements de la forme { bloc : bloc, lesson_dones : array contenant les id des lesson dones.
+         Regroupe les leçons par bloc.
+*/
+
+        function Bloc_done(lessons) {
+            console.log(lessons);
+
+            for(var i=0;i<lessons.length;i++){
+
+                var bloc=lessons[i].lessons[0].bloc;
+                console.log(lessons[i].lessons[0].bloc);
+                var lesson_done=lessons[i].lessons[0].id;
+
+                IsBlocInBlocs(bloc.id,Blocs,function(isIn){
+                    console.log(isIn);
+                    if (!(isIn.res)) {
+                        var res = {
+                            bloc: bloc,
+                            lesson_done: [lesson_done]
+                        };
+                        Blocs.push(res);
+                    } else {
+                        Blocs[isIn.num].lesson_done.push(lesson_done);
+                    }
+                    console.log(Blocs);
+                    vm.blocs = Blocs;
+                });
+
+        }
+        }
+
+        function IsBlocInBlocs(Id,Array,callback) {
+            var res = false;
+            var num = -1;
+            var done = false;
+            if (Array.length === 0) {
+                callback({res: res, num: num})
+            } else {
+                for (var i = 0; i < Array.length; i++) {
+                    if (Blocs[i].bloc.id === Id) {
+                        res = true;
+                        num = i;
+                    }
+                    console.log("Blocs:", Blocs);
+                    console.log('Id ', Id);
+                    done = (i === Array.length - 1);
+
+                    if (done) {
+                        console.log(res, num);
+                        console.log("done putain");
+                        var result = {res: res, num: num};
+                        callback(result);
+                    }
+                }
+            }
+        }
+
+        function call(text) {
+            console.log(text)
+        }
+
+
     }
 })();

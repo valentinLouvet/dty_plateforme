0807c612562a6b68c16076ee10cc0cb93835a8e6
thraxii <antoinerebeyre78@gmail.com>
2016-08-02 17:03:31 +0200

    page de création de bloc

--- nouvelle_version/src/main/webapp/app/course/create-bloc/create-bloc.controller.js
+++ nouvelle_version/src/main/webapp/app/course/create-bloc/create-bloc.controller.js
@@ -1,10 +1,49 @@
 (function () {
     'use strict';
-    
+
     angular
         .module('objectifDtyApp')
-        .controller('blocCreationController', function () {
-            
-        })
-    
+        .controller('blocCreationController',['Bloc', function (Bloc) {
+            var vm = this;
+
+            vm.blocs = [];
+
+            vm.newBloc = {
+                id: null,
+                name: "",
+                logo: "",
+                description: "",
+                is_son_of: null,
+            };
+
+            vm.saveBloc = function () {
+                console.log(vm.newBloc);
+                Bloc.save(vm.newBloc, onSaveBlocSuccess, onSaveBlocError)
+            };
+
+            function onSaveBlocSuccess() {
+                console.log('Bloc creation successful')
+            }
+
+            function onSaveBlocError(){
+                console.log('Bloc creation failed')
+            }
+
+            function loadAll(){
+                Bloc.query({}, onLoadBlocsSuccess, onLoadBlocsError)
+            }
+
+            function onLoadBlocsSuccess(data) {
+                vm.blocs = data;
+                console.log(data)
+            }
+
+            function onLoadBlocsError() {
+                console.log('Blocs loading failed')
+            }
+
+            loadAll();
+
+        }])
+
 })();
